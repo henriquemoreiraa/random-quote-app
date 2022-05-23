0017b0c73c72dca48e29df5e69dc32dcc13ea672
Henrique Moreira <riqimoreiraa@gmail.com>
2022-05-23 14:33:34 -0300

    tweet feito

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,22 @@ type State = {
 function App() {
   const [AllQuotes, setAllQuotes] = useState<State[]>([])
   const [quotes, setQuotes] = useState<State>({quote: '', author: ''})
-
+  
+  
   useEffect(() => { 
-      fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
-      .then(response => response.json())
-      .then(data => {
-        setAllQuotes(data.quotes)
+    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    .then(response => response.json())
+    .then(data => {
+      setAllQuotes(data.quotes)
+      const randomNumber = Math.floor(Math.random() * 100)
+      setQuotes(data.quotes[randomNumber])
       })  
   }, [])
-  // console.log(quotes)
-
+  
   const getRandomQuote = () => {
     const randomNumber = Math.floor(Math.random() * AllQuotes.length)
     const Quoteindex = AllQuotes[randomNumber].quote
     const Authorindex = AllQuotes[randomNumber].author
-
     setQuotes(prevQuote => ({
       ...prevQuote,
       quote: Quoteindex,
@@ -32,7 +33,12 @@ function App() {
     }))
   }
 
-  // console.log(quotes.quote)
+
+    const url = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="${quotes.quote}" ${quotes.author}`
+      
+  
+
+  console.log(AllQuotes)
 
   return (
     <C.Container >
@@ -40,7 +46,10 @@ function App() {
         <h2>{quotes.quote}</h2>
         <p>{quotes.author}</p>
 
-        <button onClick={getRandomQuote}>New quote</button>
+        <a href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="${quotes.quote}" - ${quotes.author}`} target="_blank">
+          <button>Tweet this quote</button>
+        </a>
+          <button onClick={getRandomQuote}>New quote</button>
 
       </C.Box>
     </C.Container>
