80cfb4ce3e61343d541f4c416410eace61ca33ee
Henrique Moreira <riqimoreiraa@gmail.com>
2022-05-23 18:09:45 -0300

    read me

--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 export const Container = styled.div<{ color: string }>`
     background-color: ${props => props.color};
